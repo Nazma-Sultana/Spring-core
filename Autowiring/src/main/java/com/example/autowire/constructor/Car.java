78b47e2093a39d1acf9214c94b9eb1c006fde213
unknown <nazmasultana57075@gmail.com>
2026-03-01 18:24:40 +0530

    Added autowiring

---
+++ Autowiring/src/main/java/com/example/autowire/constructor/Car.java
@@ -0,0 +1,18 @@
+
+package com.example.autowire.constructor;
+
+public class Car {
+    private Specification specification;
+//injected and set by name first finds the bean with specification and injects using setter
+    //public void setSpecification(Specification specification) {
+        //this.specification = specification;
+    //}
+
+    public Car(Specification specification) {
+        this.specification = specification;
+    }
+
+    public void displayDetails(){
+        System.out.println("Car Details:" +specification.toString());
+    }
+}
---
+++ Autowiring/src/main/java/com/example/autowire/name/Car.java
@@ -0,0 +1,14 @@
+
+package com.example.autowire.name;
+
+public class Car {
+    private Specification specification;
+//injected and set by name first finds the bean with specification and injects using setter
+    public void setSpecification(Specification specification) {
+        this.specification = specification;
+    }
+
+    public void displayDetails(){
+        System.out.println("Car Details:" +specification.toString());
+    }
+}
